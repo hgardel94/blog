1f2e9f18e5697602fd284b19c6e9b1a02a8b58af
hector <hdelgadog94@gmail.com>
2024-12-11 20:08:10 +0100

    Show notices list and detail with links

--- djangoblog/urls.py
+++ djangoblog/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from posts import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
+    path('postdetail/<int:post_id>/', views.post_detail, name='post_detail'),
 ]
