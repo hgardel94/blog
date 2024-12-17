3cad3335658126eef30549f2b877a7839ef47f11
hector <hdelgadog94@gmail.com>
2024-12-17 09:02:07 +0100

    migrations

--- posts/models.py
+++ posts/models.py
@@ -6,9 +6,17 @@ class Post(models.Model):
     title = models.CharField(max_length=80)
     content = models.TextField(blank=True)
     created = models.DateTimeField(auto_now_add=True)
-    likes = models.ManyToManyField(User, related_name='post_likes', blank=True)
+    likes = models.ManyToManyField(User, through="Like", related_name='post_likes', blank=True)
+    
     
     def __str__(self):
         return self.title
     def total_likes(self):
         return self.likes.count()
+    
+class Like(models.Model):
+    post = models.ForeignKey(Post, on_delete = models.CASCADE)
+    user = models.ForeignKey(User, on_delete= models.CASCADE)
+    date_like_it = models.DateTimeField(auto_now_add = True)
+    
+class Meta: db_table = 'posts_post_likes'
