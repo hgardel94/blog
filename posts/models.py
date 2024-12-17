13f2e465e3b5124dab0f4bca7cce7597a4fbe7d8
hector <hdelgadog94@gmail.com>
2024-12-17 09:09:31 +0100

    migrations

--- posts/models.py
+++ posts/models.py
@@ -6,9 +6,9 @@ class Post(models.Model):
     title = models.CharField(max_length=80)
     content = models.TextField(blank=True)
     created = models.DateTimeField(auto_now_add=True)
-    #likes = models.ManyToManyField(User, related_name='post_likes', blank=True)
+    likes = models.ManyToManyField(User, related_name='post_likes', blank=True)
     
     def __str__(self):
         return self.title
-   # def total_likes(self):
-    #    return self.likes.count()
+    def total_likes(self):
+        return self.likes.count()
